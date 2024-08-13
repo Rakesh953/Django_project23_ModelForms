16b8a479bc22c3f70afc81b607f34fa46cf526d9
@Rakesh953 <rakeshmeher953@gmail.com>
2024-08-13 16:08:58 +0530

    first commit

---
+++ app/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from app.models import *
+
+class TopicForms(forms.Form):
+    topicname=forms.CharField(max_length=100)
+
+
+class WebpageForm(forms.Form):
+    topicname=forms.ModelChoiceField(queryset=Topic.objects.all())
+    name=forms.CharField(max_length=100)
+    url=forms.URLField()
+
+class TopicModelForm(forms.ModelForm):
+    class Meta:
+        model=Topic
+        fields='__all__'
+
+class WebpageModelForm(forms.ModelForm):
+    class Meta:
+        model=Webpage
+        #fields='__all__'
+        #fields=['topic_name','name']
+        exclude=['url']
